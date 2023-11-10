3d7cecc98fca073a8d8fcc7f04cdeacfbeaf0124
mallus rgreat <freedomgamingyt@gmail.com>
2023-11-10 23:20:55 +0530

    w

---
+++ bot.py
@@ -0,0 +1,26 @@
+from colorama import Fore
+from urllib import parse
+import discord
+from discord import app_commands
+import config
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"{Fore.GREEN}[ONLINE] {Fore.CYAN}{bot.user.name}#{bot.user.discriminator} {Fore.green}is online.")
+
+@bot.hybrid_command(name="ping", description="Check the bot's ping")
+async def pingCmd(ctx: commands.Context):
+    await ctx.send(f"{round(bot.latency)}ms ping.")
+
+@bot.hybrid_command(name="google", description="Google")
+@app_commands.describe(
+    text = "The text to google"
+)
+async def googleCmd(ctx: commands.Context, text: str):
+    await ctx.send(f"https://google.com/search?q={parse.urlencode(text.replace(' ', '+'))}")
+
+bot.run(config.TOKEN)
