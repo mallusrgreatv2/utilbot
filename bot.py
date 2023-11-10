83e798a5147aeed9051e2f276d90ed102f9b299a
mallus rgreat <freedomgamingyt@gmail.com>
2023-11-10 23:41:05 +0530

    Add descriptions to commands

--- bot.py
+++ bot.py
@@ -12,15 +12,17 @@ async def on_ready():
     await bot.tree.sync()
     print(f"{Fore.GREEN}[ONLINE] {Fore.CYAN}{bot.user.name}#{bot.user.discriminator} {Fore.GREEN}is online.")
 
-@bot.hybrid_command(name="ping", description="Check the bot's ping")
+@bot.hybrid_command(name="ping")
 async def pingCmd(ctx: commands.Context):
+    """Check the bot's ping"""
     await ctx.send(f"{round(bot.latency)}ms ping.")
 
-@bot.hybrid_command(name="google", description="Google")
+@bot.hybrid_command(name="google")
 @app_commands.describe(
     text = "The text to google"
 )
 async def googleCmd(ctx: commands.Context, *text: str):
+    """Generate a google link searching for the given text"""
     await ctx.send(f"https://google.com/search?q={parse.quote('+'.join(text))}")
 
 bot.run(config.TOKEN)
